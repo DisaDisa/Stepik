035dec98e43243046bc939d0c1884c471fd0a335
DisaDisa <denisarhipov97@gmail.com>
2018-07-05 08:16:52 +0300

    Rational

---
+++ Rational/Rational3.cpp
@@ -0,0 +1,78 @@
+struct Rational
+{
+    Rational(int numerator = 0, int denominator = 1);
+
+    void add(Rational rational);
+    void sub(Rational rational);
+    void mul(Rational rational);
+    void div(Rational rational);
+
+    void neg();
+    void inv();
+    double to_double() const;
+
+    Rational& operator+=(Rational rational);
+    Rational& operator-=(Rational rational);
+    Rational& operator*=(Rational rational);
+    Rational& operator/=(Rational rational);
+
+    Rational operator-() const;
+    Rational operator+() const;
+    
+
+private:
+    int numerator_;
+    int denominator_;
+    
+    friend bool operator==(Rational const&, Rational const&);
+    friend bool operator!=(Rational const&, Rational const&);
+    friend bool operator<(Rational const&, Rational const&);
+    friend bool operator>(Rational const&, Rational const&);
+    friend bool operator<=(Rational const&, Rational const&);
+    friend bool operator>=(Rational const&, Rational const&);
+    
+};
+
+int nod(int n, int m) {
+    while (n != 0 && m != 0) {
+        n %= m;
+        int k = n;
+        n = m;
+        m = k;
+    }
+    return n + m;
+}
+    
+int nok(int n, int m) {
+    return n / nod(n, m) * m;
+}
+
+Rational operator+(Rational lhs, Rational rhs);
+Rational operator-(Rational lhs, Rational rhs);
+Rational operator*(Rational lhs, Rational rhs);
+Rational operator/(Rational lhs, Rational rhs);
+bool operator==(Rational const& r1, Rational const& r2) {
+    return r1.numerator_ * r2.denominator_ == r1.denominator_ * r2.numerator_;
+}
+
+bool operator!=(Rational const& r1, Rational const& r2) {
+    return !(r1 == r2);
+}
+
+bool operator<(Rational const& r1, Rational const& r2) {
+    int NOK = nok(r1.denominator_, r2.denominator_);
+    return r1.numerator_ * (NOK / r1.denominator_) < r2.numerator_ * (NOK / r2.denominator_);
+}
+
+bool operator>(Rational const& r1, Rational const& r2) {
+    return r2 < r1;
+}
+
+bool operator<=(Rational const& r1, Rational const& r2) {
+    return !(r1 > r2);
+}
+
+bool operator>=(Rational const& r1, Rational const& r2) {
+    return !(r1 < r2);
+}
+    
